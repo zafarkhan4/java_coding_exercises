b36b83a4bdaf5df41459e749c9cb1118f7a2f2c9
James Heggs <james.heggs@techreturners.com>
2020-08-24 19:34:19 +0100

    Exercise for object creation

---
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -0,0 +1,13 @@
+package com.techreturners.exercise002;
+
+public class Exercise002 {
+
+    public boolean isFromManchester(/* Person person */) {
+        return false;
+    }
+
+    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
+        return false;
+    }
+    
+}
---
+++ src/test/java/com/techreturners/exercise002/Exercise002Test.java
@@ -0,0 +1,29 @@
+package com.techreturners.exercise002;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Exercise002Test {
+
+    @Test
+    public void checkIsFromManchester() {
+        Exercise002 ex002 = new Exercise002();
+
+        //Person p1 = new Person("Peter", "Smith", "Manchester", 23);
+        //Person p2 = new Person("Susan", "Farmer", "Leeds", 23);
+
+        assertEquals(true, ex002.isFromManchester(/* p1 */ ));
+        assertEquals(false, ex002.isFromManchester(/* p2 */ ));
+    }
+
+    public void checkCanWatchFilm() {
+        Exercise002 ex002 = new Exercise002();
+
+        //Person p1 = new Person("Peter", "Smith", "Manchester", 17);
+        //Person p2 = new Person("Francis", "Farmer", "Leeds", 18);
+
+        assertEquals(false, ex002.canWatchFilm(/* p1, 18 */ ));
+        assertEquals(true, ex002.canWatchFilm(/* p2, 18 */ ));
+    }
+}
