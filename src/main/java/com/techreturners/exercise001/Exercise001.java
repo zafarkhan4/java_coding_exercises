e8f73a540ecb1da8015f362aa514de27e26f1626
EllieJudge <47675388+EllieJudge@users.noreply.github.com>
2020-11-22 15:47:59 +0000

    Update Exercise001.java

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -9,18 +9,22 @@ public class Exercise001 {
     }
 
     public String generateInitials(String firstName, String lastName) {
+        // Add your code here
         return "";
     }
 
     public double addVat(double originalPrice, double vatRate) {
+        // Add your code here
         return 0.0;
     }
 
     public String reverse(String sentence) {
+        // Add your code here
         return "";
     }
 
     public int countLinuxUsers(List<User> users) {
+        // Add your code here
         return 0;
     }
 }
