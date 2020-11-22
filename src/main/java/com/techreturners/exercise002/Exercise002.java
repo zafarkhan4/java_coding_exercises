f4d6053d19002d48f3808da26128630d14c0ec9d
EllieJudge <47675388+EllieJudge@users.noreply.github.com>
2020-11-22 15:48:20 +0000

    Update Exercise002.java

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -3,10 +3,12 @@ package com.techreturners.exercise002;
 public class Exercise002 {
 
     public boolean isFromManchester(/* Person person */) {
+        // Add your code here
         return false;
     }
 
     public boolean canWatchFilm(/* Person person, int ageLimit*/) {
+        // Add your code here
         return false;
     }
     
